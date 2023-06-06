7e23bc323ef879980b4f28479dedbe189db010d2
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2023-06-06 15:34:40 +0530

    deployement

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/user",userRouter);
+app.use("/api/posts",postRouter);
+
 app.use("/",(req,res)=>{
     res.json("Home page")
 })
-app.use("/api/user",userRouter);
-app.use("/api/posts",postRouter);
 
 app.listen(process.env.port, async () => {
     try {
